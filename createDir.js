56bfee0f9879d8a308811d8240bccd085f26240d
Olha Vinnikova <vinnikova@online.ua>
2019-04-02 00:13:04 +0300

    task 5

--- createDir.js
+++ createDir.js
@@ -1,11 +1,11 @@
 //task 4
-var fs = require('file-system');
+const fs = require('file-system');
 const date = new Date();
-fs.mkdir('./newDir', function(err) {
+fs.mkdir('./newDir', (err) => {
     if (err) console.error(err)
         else console.log('Done!')
 });
-fs.writeFile('./newDir/newfile.txt', date , function (err) {
+fs.writeFile('./newDir/newfile.txt', date , (err) => {
     if (err) throw err;
     console.log('Saved!');
   });
