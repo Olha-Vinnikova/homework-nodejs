a225e07f78157eb0ba12c68a9539373997960674
Olha Vinnikova <olha.vinnikova@dish.com>
2019-04-01 19:37:21 +0300

    task 3

---
+++ httpGet.js
@@ -0,0 +1,23 @@
+const https = require('https');
+
+const httpGet = (url) => new Promise((resolve, reject) => {
+    https.get(url, (resp) => {
+        let data = '';
+
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        resp.on('end', () => {
+            resolve(JSON.parse(data));
+        });
+
+    }).on("error", (error) => {
+        reject(error);
+        return;
+    });
+});
+
+module.exports = httpGet;
+
+
--- main.js
+++ main.js
@@ -2,14 +2,21 @@ const randome = require('./random');
 
 //task 1
 const currentDate = new Date();
-const hours = currentDate.getHours()<10? `0${currentDate.getHours()}` : currentDate.getHours();
-const minutes = currentDate.getMinutes()<10? `0${currentDate.getMinutes()}` : currentDate.getMinutes();
-const day = currentDate.getDate()<10? `0${currentDate.getDate()}` : currentDate.getDate();
-const month = currentDate.getMonth()<10? `0${currentDate.getMonth()}` : currentDate.getMonth();
+const hours = currentDate.getHours() < 10 ? `0${currentDate.getHours()}` : currentDate.getHours();
+const minutes = currentDate.getMinutes() < 10 ? `0${currentDate.getMinutes()}` : currentDate.getMinutes();
+const day = currentDate.getDate() < 10 ? `0${currentDate.getDate()}` : currentDate.getDate();
+const month = currentDate.getMonth() < 10 ? `0${currentDate.getMonth()}` : currentDate.getMonth();
 console.log(`Date: ${hours}:${minutes}  ${day}.${month}.${currentDate.getFullYear()}`)
 
 const fileName = process.argv[1].includes('/') ? process.argv[1].split('/').pop() : process.argv[1].split('\\').pop();
 console.log('File:', fileName);
 
 //task 2
-console.log('Random number: ', randome(1,10))
+console.log('Random number: ', randome(1, 10))
+
+//task 3
+const httpGet = require('./httpGet');
+
+httpGet('https://jsonplaceholder.typicode.com/posts')
+    .then(response => console.log('httpGet:', response.length)) // --> 100
+    .catch(err => console.log(err));
